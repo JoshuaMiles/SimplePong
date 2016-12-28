e3c19a5bd6cb962c8dfe1da32d3a52104c1c42ea
josh <josh@DESKTOP-QQ9FAN3>
2016-12-28 16:34:27 +1000

    still working on player movement

---
+++ SimplePong/ObjectFunctions.fs
@@ -0,0 +1,87 @@
+﻿
+module ObjectFunctions
+open System.Windows.Forms
+
+
+let WIDTH = 600
+let HEIGHT = 400
+let SCALE = 10
+
+
+type P(x: int, y: int) =
+    member this.X = x
+    member this.Y = y
+
+type Direction(dy: float, dx: float) =
+    member this.DY = dy
+    member this.DX = dx
+
+type GameObject = {pos: P ; dir : Direction; height: int; width: int}
+
+let round (x:float) = int (System.Math.Round x)
+
+let startingAngle (dir: Direction,degrees:float) =
+    let radians = degrees *  (System.Math.PI / 180.0)
+    let s = System.Math.Sin radians
+    let c = System.Math.Cos radians
+    let dx = c * dir.DX + s * dir.DY 
+    let dy = -s * dir.DX + c * dir.DY
+    Direction(dy,dx)
+
+// A function that takes the game object and than applies the DY and DX variables to the current position effectivly moving it
+let moveObject (gameBall : GameObject) =   
+    { pos = P(gameBall.pos.X + round gameBall.dir.DX, gameBall.pos.Y + round gameBall.dir.DY); 
+    dir = Direction(gameBall.dir.DX, gameBall.dir.DY); width = gameBall.width; height = gameBall.height}
+
+let bounce (gameBall: GameObject) =
+    let checkBounds min max currentPos dir =
+        match currentPos with
+            | currentPos when currentPos <= min -> -dir
+            | currentPos when currentPos > max - 10 -> -dir
+            | _ -> dir
+    let dx = checkBounds 0 WIDTH gameBall.pos.X gameBall.dir.DY
+    let dy = checkBounds 0 HEIGHT gameBall.pos.Y gameBall.dir.DX
+    // Less graceful solution but a common problem with immutability
+    {pos = P(gameBall.pos.X,gameBall.pos.Y); dir = Direction(dy, dx); height = gameBall.height; width = gameBall.width }
+
+let collision (gameBall: GameObject, player : GameObject) =
+    let gameBallLeft = gameBall.pos.X
+    let gameBallRight = gameBallLeft + gameBall.width
+    let gameBallTop = gameBall.pos.Y
+    let gameBallBottom = gameBallTop + gameBall.height
+
+    let playerLeft = player.pos.X
+    let playerRight = playerLeft + player.width
+    let playerTop = player.pos.Y
+    let playerBottom = playerTop + player.height
+
+    gameBallBottom > playerTop && 
+    gameBallTop < playerBottom &&
+    gameBallLeft < playerRight &&
+    gameBallRight > playerLeft 
+
+let checkAndLetTheObjectBounce (gameBall: GameObject, player1: GameObject, player2) =
+    if collision (gameBall, player1) || collision (gameBall, player2)
+    then {pos = P(gameBall.pos.X, gameBall.pos.Y); dir = Direction(gameBall.dir.DY, -gameBall.dir.DX); height = gameBall.height; width = gameBall.width }
+    else  gameBall
+
+
+
+let keyToDir = function
+    | 38 -> Some(Direction(20.0,0.0))
+    | 40 -> Some(Direction(-20.0,0.0))
+    | _  -> None
+
+let mutable newDir = Direction(0.0,0.0)
+
+// KeyEventArgs => unit
+let movePlayer(e: KeyEventArgs) =
+    let dir = keyToDir e.KeyValue
+    if Option.isSome dir
+    then Option.get dir
+    else Direction(0.0,0.0)
+
+
+// GameObject => Direction => GameObject
+let combiningPlayerAndDirection(player: GameObject, newDirection: Direction) = 
+   {pos = player.pos ; dir = newDirection ; height = player.height ; width = player.width}
--- SimplePong/Program.fs
+++ SimplePong/Program.fs
@@ -1,12 +1,8 @@
 ﻿open System
 open System.Windows.Forms
 open System.Drawing
+open ObjectFunctions
 open PlayerFunctions
-open BallFunctions
-
-let WIDTH = 600
-let HEIGHT = 400
-let SCALE = 10
 
 type myForm() =
     inherit Form()
@@ -15,12 +11,6 @@ type myForm() =
 let form = new myForm(Text = "Pong", ClientSize = Size(WIDTH, HEIGHT))
 
 let gr = form.CreateGraphics()
-
-let round (x:float) = int (System.Math.Round x)
-
-let player1 =  { pos = P(50,50) ; dir = Direction(5.0,5.0) ; height = 100 ; width = 20 }
-
-let player2 = { pos = P(250,250) ; dir = Direction(5.0,5.0); height = 100 ; width = 20 }
 
 let drawDebugStrings (x:int, y:int, dy:float, dx: float, playerOrBall:String) = 
     use blue = new SolidBrush(Color.Blue)
@@ -31,51 +21,48 @@ let drawDebugStrings (x:int, y:int, dy:float, dx: float, playerOrBall:String) =
     gr.DrawString(playerOrBall + " dy: " + dy.ToString(), font, blue, PointF(100.0f,0.0f))
     gr.DrawString(playerOrBall + " dx: " + dx.ToString(), font, blue, PointF(100.0f,25.0f))
 
-let debuggerDraw (gameBall: BallFunctions.GameObject, player1: BallFunctions.GameObject, player2 : BallFunctions.GameObject, e: KeyEventArgs) =
+let debuggerDraw (gameBall: ObjectFunctions.GameObject, player1: ObjectFunctions.GameObject, player2 : ObjectFunctions.GameObject, e: KeyEventArgs) =
     match e.KeyValue with 
     | 38 ->  drawDebugStrings(gameBall.pos.X,gameBall.pos.Y, gameBall.dir.DY, gameBall.dir.DX, "Ball")
     | 40 ->  drawDebugStrings(gameBall.pos.X,gameBall.pos.Y, gameBall.dir.DY, gameBall.dir.DX, "Player 1")
     | 37 ->  drawDebugStrings(player2.pos.X,player2.pos.Y, player2.dir.DY, player2.dir.DX, "Player 2")
     | _ ->  drawDebugStrings(0,0,0.0,0.0,"")
 
-let draw (gameBall: BallFunctions.GameObject) =
+let draw (gameBall: ObjectFunctions.GameObject) =
     use blue = new SolidBrush(Color.Blue)
     use black = new SolidBrush(Color.Black)
     use font = new Font("Arial", 16.0f)
     gr.FillRectangle(black, 0, 0, WIDTH, HEIGHT)
     gr.FillRectangle(blue, gameBall.pos.X, gameBall.pos.Y, gameBall.height,gameBall.width)
 
-let drawPlayers (player1:BallFunctions.GameObject,player2:BallFunctions.GameObject) =
+let drawPlayers (player1:ObjectFunctions.GameObject,player2:ObjectFunctions.GameObject) =
     use blue = new SolidBrush(Color.Blue)
     use black = new SolidBrush(Color.Black)
     gr.FillRectangle(blue, player1.pos.X, player1.pos.Y, 10, 10)
     gr.FillRectangle(blue, player2.pos.X, player2.pos.Y, 10, 10)
 
 
-let startingAngle (dir: BallFunctions.Direction,degrees:float) =
-    let radians = degrees *  (System.Math.PI / 180.0)
-    let s = System.Math.Sin radians
-    let c = System.Math.Cos radians
-    let dx = c * dir.DX + s * dir.DY 
-    let dy = -s * dir.DX + c * dir.DY
-    BallFunctions.Direction(dy,dx)
 
-
-let moveBall (gameBall : BallFunctions.GameObject) =   
-    { pos = P(gameBall.pos.X + round gameBall.dir.DX, gameBall.pos.Y + round gameBall.dir.DY); 
-    dir = Direction(gameBall.dir.DX, gameBall.dir.DY); width = gameBall.width; height = gameBall.height}
-
-let rec gameLoop((gameBall: BallFunctions.GameObject , player1: BallFunctions.GameObject , player2 : BallFunctions.GameObject)) = async {
+// The primary loop that the game keeps repeating, acting like the 'motor' of the program
+let rec gameLoop((gameBall: ObjectFunctions.GameObject , player1: ObjectFunctions.GameObject , player2 : ObjectFunctions.GameObject)) = async {
     let now = DateTime.Now
     let dt = 64.0 - DateTime.Now.Subtract(now).TotalMilliseconds
     // Apply the function to change the position of the ball to the ball
-    let newBall = gameBall |> moveBall |> bounce
+    let newBall = checkAndLetTheObjectBounce (gameBall, player1, player2) |> moveObject |> bounce 
+    let mutable something
+    // Current problem, the Objservable.add function needs to take a function which returns unit (which is just nothing)
+    // Need to make a function which creates a side effect that changes the direction of the paddle from the input
+    let newPlayerMovement = form.KeyDown |> Observable.add something
     draw newBall
     drawPlayers (player1, player2)
     do! Async.Sleep(Math.Max(dt, 0.0) |> int)
     return! gameLoop(newBall, player1, player2) }
 
 let gameBall = {pos = P((WIDTH/2)-5,(HEIGHT/2)-5); dir = startingAngle(Direction(5.0,5.0), 30.0) ; height = 10; width = 10 }
+
+let player1 =  { pos = P(50,50) ; dir = Direction(5.0,5.0) ; height = 100 ; width = 20 }
+
+let player2 = { pos = P(550,250) ; dir = Direction(5.0,5.0); height = 100 ; width = 20 }
 
 [<STAThread>]
 do Async.Start(gameLoop(gameBall, player1, player2))
